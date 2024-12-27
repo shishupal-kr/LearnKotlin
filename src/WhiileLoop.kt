42d73d5bf46445732c2391d6929bdf9119811b8b
Shishupal <shishupalking@gmail.com>
2024-12-27 19:22:13 +0530

    coded

---
+++ src/WhiileLoop.kt
@@ -0,0 +1,25 @@
+fun main(){
+
+
+    //While loop
+    var i = 0
+    while (i <= 5){
+        println(i)
+        i++
+    }
+
+    //2nd example
+    var j = 0
+    while (j <= 3){
+        println("Shishupal")
+        j ++
+    }
+
+
+    //do while loop , it will execute at least once before checking the condition after running the loop
+    var k = 0
+    do {
+        println(k)
+        k++
+    } while (k <= 5)
+}
