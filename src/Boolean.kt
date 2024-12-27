046ebaf3437c341facbddfa4d6c0e3456045c57f
Shishupal <shishupalking@gmail.com>
2024-12-27 18:43:32 +0530

    bool code added

---
+++ src/Boolean.kt
@@ -0,0 +1,26 @@
+fun main() {
+
+    //Boolean Values
+    val isTrue: Boolean = true
+    val isFalse: Boolean = false
+
+    println(isTrue)
+    println(isFalse)
+
+
+    var rain = true
+    var snow = false
+    println(rain)
+    println(snow)
+
+
+    //Expression
+    var a = 10
+    var b = 5
+
+    println(a>b)
+    println(a<b)
+
+
+
+}
